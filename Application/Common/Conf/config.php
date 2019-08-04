4388ed5a7a132cb3d1608aac9b1bf768a65538f9
longteng <1774341213@qq.com>
2019-08-04 19:23:22 +0800

    aa

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -42,7 +42,7 @@ return array(
     'DB_TYPE'   => 'mysqli', // 数据库类型
     'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'fayuanb', // 数据库名
-    'DB_USER'   => 'root', // 用户名
+    'DB_USER'   => 'admin', // 用户名
     'DB_PWD'    => 'root',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'bhy_', // 数据库表前缀
